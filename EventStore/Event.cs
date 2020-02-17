20ef6cf2ae266147d57f1c5a250ba89fbadbc5ad
mhumelsine <mhumelsine@gmail.com>
2020-02-17 14:38:55 -0500

    initial

---
+++ EventStore/Event.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventStore
+{
+    public abstract class Event
+    {
+        public Guid Uid { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
